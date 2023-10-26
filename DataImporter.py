ce6880b364df44b17de6436303eb369cbe593272
trallen22 <77846813+trallen22@users.noreply.github.com>
2023-10-26 14:14:14 -0400

    moved connection.commit() to commit once at the end of the file

--- DataImporter.py
+++ DataImporter.py
@@ -41,13 +41,10 @@ def createSchema(connection):
 def sqlInsert(table, curTuple):
 	if table == 'plays':
 		sqlStr = f'INSERT INTO plays VALUES (%s, %s, %s, %s, %s, %s, %s);'
-	# elif table == 'matches':
-	# 	sqlStr = f'INSERT INTO {table} VALUES (%s, %s, %s, %s, %s);'
 	else: 
 		sqlStr = f'INSERT INTO {table} VALUES (%s, %s, %s, %s, %s, %s);'
 
 	cursor.execute(sqlStr, curTuple)
-	connection.commit()
 
 
 ################
@@ -57,14 +54,13 @@ def sqlInsert(table, curTuple):
 i = 1 
 
 try: 
-	connection = mysql.connector.connect(host=HOST, user=USER, password='123456', database="Tennis") 
+	connection = mysql.connector.connect(host=HOST, user=USER, database="Tennis") 
 except Exception as e:
 	print(f'error: {e}')
 
 # running the Schema.sql 
 # createSchema(connection)
 cursor = connection.cursor()
-
 
 print(os.getcwd())
 
@@ -77,7 +73,7 @@ print(f'starting to parse files')
 for filename in glob.glob(f"{os.getcwd()}/tennis_atp/atp_matches_????.csv"):
 	print(f'parsing file {i}')
 	i += 1
-
+	sqlScript = ""
 	with open(filename, 'r') as curFile:
 		curCsv = csv.DictReader(curFile)
 		for row in curCsv:
@@ -89,7 +85,7 @@ for filename in glob.glob(f"{os.getcwd()}/tennis_atp/atp_matches_????.csv"):
 								(row['draw_size'] if not row['draw_size'] == '' else 0), 
 								row['tourney_level'], row['tourney_date'])
 
-				sqlInsert('tournament', curTourneyTuple)            
+				sqlInsert('tournament', curTourneyTuple)      
 				tourneyIdSet.add(row['tourney_id'])
 
 			if not row['winner_id'] in playerIdSet:
@@ -97,6 +93,7 @@ for filename in glob.glob(f"{os.getcwd()}/tennis_atp/atp_matches_????.csv"):
 								(row['winner_ht'] if not row['winner_ht'] == '' else 0),
 								row['winner_ioc'], 
 								(row['winner_age'] if not row['winner_age'] == '' else 0))
+				
 				sqlInsert('player', curPlayerTuple)
 				playerIdSet.add(row['winner_id'])
 
@@ -105,6 +102,7 @@ for filename in glob.glob(f"{os.getcwd()}/tennis_atp/atp_matches_????.csv"):
 								(row['loser_ht'] if not row['loser_ht'] == '' else 0), 
 								row['loser_ioc'], 
 								(row['loser_age'] if not row['loser_age'] == '' else 0))
+
 				sqlInsert('player', curPlayerTuple)
 				playerIdSet.add(row['loser_id'])
 			
@@ -112,6 +110,7 @@ for filename in glob.glob(f"{os.getcwd()}/tennis_atp/atp_matches_????.csv"):
 				curMatchTuple = (row['tourney_id'], row['match_num'], row['score'], 
 								row['best_of'], row['round'], 
 								(row['minutes'] if not row['minutes'] == '' else 0))
+
 				sqlInsert('matches', curMatchTuple)
 				matchIdSet.add((row['tourney_id'], row['match_num']))
 
@@ -126,8 +125,11 @@ for filename in glob.glob(f"{os.getcwd()}/tennis_atp/atp_matches_????.csv"):
 											(row['l_df'] if not row['l_df'] == '' else 0), 
 											(row['l_svpt'] if not row['l_svpt'] == '' else 0), 
 											(row['l_bpSaved'] if not row['l_bpSaved'] == '' else 0))
+
 				sqlInsert('plays', curWinnerTuple)
 				sqlInsert('plays', curLoserTuple)
 				playsIdSet.add((row['tourney_id'], row['match_num']))
+
+	connection.commit()
 
 cursor.close()
