33fa20a61a7f4e0c84734dd48e3917a33dd8554f
trallen22 <77846813+trallen22@users.noreply.github.com>
2023-10-28 00:19:52 -0400

    added function and view

--- DynamicCalculations.sql
+++ DynamicCalculations.sql
@@ -1,44 +1,50 @@
 USE Tennis;
 
-CREATE FUNCTION aceCount(name, start, finish)
-RETURNS DOUBLE
+-- DROP FUNCTION IF EXISTS aceCount;
+-- DELIMITER //
+-- CREATE FUNCTION aceCount(name VARCHAR(50), start INT, finish INT) 
+-- RETURNS DOUBLE 
+-- DETERMINISTIC
+-- BEGIN 
+--     DECLARE average DOUBLE;
+    
+--     SELECT AVG(T2.ace) INTO average
+--         FROM player AS cp
+--         JOIN (SELECT pl.player_id, pl.ace 
+--             FROM plays as pl 
+--             JOIN (SELECT t.tourney_date, m.match_id 
+--                 FROM tournament AS t 
+--                 JOIN matches AS m ON t.tournament_id=m.tournament_id
+--                 WHERE t.tourney_date > start
+--                     AND t.tourney_date < finish) AS T1 ON pl.match_id=T1.match_id) 
+--                 AS T2 ON cp.player_id=T2.player_id
+--         WHERE cp.name=name;
 
+--     RETURN average;
+-- END;
+-- // 
+-- DELIMITER ;
+
+-- SELECT aceCount('Rafael Nadal', 20180000, 20220000);
+
+
+-- DROP VIEW IF EXISTS topAces;
+-- CREATE VIEW topAces AS 
+-- SELECT p.name, SUM(m.ace) AS total_aces
+-- FROM plays AS m 
+-- JOIN player AS p ON m.player_id=p.player_id
+-- GROUP BY m.player_id
+-- ORDER BY SUM(m.ace) DESC
+-- LIMIT 10;
+
+-- SELECT * FROM topAces;
+
+DROP TRIGGER IF EXISTS onInsertionPlayer;
+CREATE TRIGGER onInsertionPlayer 
+BEFORE INSERT ON player 
+FOR EACH ROW 
 BEGIN 
-
-DECLARE average DOUBLE;
-
-SET average = (SELECT AVG(T2.ace) INTO average
-    FROM player AS cp
-    JOIN (SELECT pl.player_id, pl.ace 
-        FROM plays as pl 
-        JOIN (SELECT t.tourney_date, m.match_id 
-            FROM tournament AS t 
-            JOIN matches AS m ON t.tournament_id=m.tournament_id
-            WHERE t.tourney_date > start
-                AND t.tourney_date < finish) AS T1 ON pl.match_id=T1.match_id) 
-            AS T2 ON cp.player_id=T2.player_id
-    WHERE cp.name=name);
-
-RETURN average;
-
-END;
-
-
--- combining tournament and matches 
--- SELECT t.tourney_date, m.match_id 
---     FROM tournament AS t 
---     JOIN matches AS m ON t.tournament_id=m.tournament_id
---     WHERE t.tourney_date > 20180000 
---         AND t.tourney_date < 20220000
-
--- getting match data from certain matches 
--- SELECT pl.player_id, pl.ace 
---     FROM plays AS pl 
---     JOIN (SELECT t.tourney_date, m.match_id 
---         FROM tournament AS t 
---         JOIN matches AS m ON t.tournament_id=m.tournament_id
---         WHERE t.tourney_date > 20180000 
---             AND t.tourney_date < 20220000) AS T1 ON pl.match_id=T1.match_id
-
-
-SELECT aceCount('Rafael Nadal', 20180000, 20220000);
+    IF player.ioc='RUS' OR player.ioc='EST'
+    THEN SET player.ioc='USR'
+    END IF;
+END 
