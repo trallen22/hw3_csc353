3cb28d2c504825b780b85bb605b9c88e5cec7b69
trallen22 <77846813+trallen22@users.noreply.github.com>
2023-10-30 12:44:22 -0400

    changed tourney_date to type DATE in schema

--- DataImporter.py
+++ DataImporter.py
@@ -64,7 +64,7 @@ except Exception as e:
 
 cursor = connection.cursor()
 
-# createSchema(connection)
+createSchema(connection)
 
 tourneyIdSet = set() 
 playerIdSet = set()
@@ -157,12 +157,12 @@ for filename in glob.glob(f"{os.getcwd()}/tennis_atp/atp_matches_????.csv"):
 				sqlInsert('plays', curWinnerTuple)
 				sqlInsert('plays', curLoserTuple)
 				playsIdSet.add((row['tourney_id'], row['match_num']))
-		# 	x += 1
-		# 	if x == 5:
-		# 		break
-		# y += 1
-		# if y == 5:
-		# 	break
+			x += 1
+			if x == 5:
+				break
+		y += 1
+		if y == 5:
+			break
 
 connection.commit()
 
--- DynamicCalculations.sql
+++ DynamicCalculations.sql
@@ -1,83 +1,82 @@
 USE Tennis;
--- DROP SCHEMA Tennis;
 
--- DROP FUNCTION IF EXISTS aceCount;
--- DELIMITER //
--- CREATE FUNCTION aceCount(name VARCHAR(50), start INT, finish INT) 
--- RETURNS DOUBLE 
--- DETERMINISTIC
--- BEGIN 
---     DECLARE average DOUBLE;
+DROP FUNCTION IF EXISTS aceCount;
+DELIMITER //
+CREATE FUNCTION aceCount(name VARCHAR(50), start DATE, finish DATE) 
+RETURNS DOUBLE 
+DETERMINISTIC
+BEGIN 
+    DECLARE average DOUBLE;
     
---     SELECT AVG(T2.ace) INTO average
---         FROM player AS cp
---         JOIN (SELECT pl.player_id, pl.ace 
---             FROM plays as pl 
---             JOIN (SELECT t.tourney_date, m.match_id 
---                 FROM tournament AS t 
---                 JOIN matches AS m ON t.tournament_id=m.tournament_id
---                 WHERE t.tourney_date > start
---                     AND t.tourney_date < finish) AS T1 ON pl.match_id=T1.match_id) 
---                 AS T2 ON cp.player_id=T2.player_id
---         WHERE cp.name=name;
+    SELECT AVG(T2.ace) INTO average
+        FROM player AS cp
+        JOIN (SELECT pl.player_id, pl.ace 
+            FROM plays as pl 
+            JOIN (SELECT t.tourney_date, m.match_id 
+                FROM tournament AS t 
+                JOIN matches AS m ON t.tournament_id=m.tournament_id
+                WHERE t.tourney_date > start
+                    AND t.tourney_date < finish) AS T1 ON pl.match_id=T1.match_id) 
+                AS T2 ON cp.player_id=T2.player_id
+        WHERE cp.name=name;
 
---     RETURN average;
--- END;
--- // 
--- DELIMITER ;
+    RETURN average;
+END;
+// 
+DELIMITER ;
 
--- SELECT aceCount('Rafael Nadal', 20180000, 20220000);
-
-
-
--- DROP PROCEDURE IF EXISTS showAggregateStatistics;
--- DELIMITER // 
--- CREATE PROCEDURE showAggregateStatistics(name VARCHAR(50), start INT, finish INT)
--- BEGIN
---     SELECT cp.name, AVG(P1.ace) AS aces_per_match, AVG(P1.dbl_fault) AS dbl_faults_per_match, 
---         AVG(P1.serve_point) AS serve_points_per_match, AVG(P1.brk_points_saved) AS brk_points_saved_per_match
---     FROM player AS cp
---     JOIN (SELECT pl.player_id, pl.ace, pl.dbl_fault, pl.serve_point, pl.brk_points_saved 
---         FROM plays as pl 
---         JOIN (SELECT t.tourney_date, m.match_id 
---             FROM tournament AS t 
---             JOIN matches AS m ON t.tournament_id=m.tournament_id
---             WHERE t.tourney_date > start AND t.tourney_date < finish) 
---             AS T1 ON pl.match_id=T1.match_id) 
---         AS P1 ON cp.player_id=P1.player_id
---     WHERE cp.name=name
---     GROUP BY cp.name;    
--- END;
--- // 
--- DELIMITER ;
-
--- CALL showAggregateStatistics('Rafael Nadal', 20180000, 20210000);
+SELECT aceCount('Rafael Nadal', '20180101', '20220101');
 
 
 
--- DROP VIEW IF EXISTS topAces;
--- CREATE VIEW topAces AS 
--- SELECT p.name, SUM(m.ace) AS total_aces
--- FROM plays AS m 
--- JOIN player AS p ON m.player_id=p.player_id
--- GROUP BY m.player_id
--- ORDER BY SUM(m.ace) DESC
--- LIMIT 10;
+DROP PROCEDURE IF EXISTS showAggregateStatistics;
+DELIMITER // 
+CREATE PROCEDURE showAggregateStatistics(name VARCHAR(50), start DATE, finish DATE)
+BEGIN
+    SELECT cp.name, AVG(P1.ace) AS aces_per_match, AVG(P1.dbl_fault) AS dbl_faults_per_match, 
+        AVG(P1.serve_point) AS serve_points_per_match, AVG(P1.brk_points_saved) AS brk_points_saved_per_match
+    FROM player AS cp
+    JOIN (SELECT pl.player_id, pl.ace, pl.dbl_fault, pl.serve_point, pl.brk_points_saved 
+        FROM plays as pl 
+        JOIN (SELECT t.tourney_date, m.match_id 
+            FROM tournament AS t 
+            JOIN matches AS m ON t.tournament_id=m.tournament_id
+            WHERE t.tourney_date > start AND t.tourney_date < finish) 
+            AS T1 ON pl.match_id=T1.match_id) 
+        AS P1 ON cp.player_id=P1.player_id
+    WHERE cp.name=name
+    GROUP BY cp.name;    
+END;
+// 
+DELIMITER ;
 
--- SELECT * FROM topAces;
+CALL showAggregateStatistics('Rafael Nadal', '20180101', '20210101');
 
 
 
--- DROP TRIGGER IF EXISTS onInsertionPlayer;
--- DELIMITER // 
--- CREATE TRIGGER onInsertionPlayer 
--- BEFORE INSERT ON player 
--- FOR EACH ROW 
--- BEGIN 
---     IF NEW.ioc='RUS' OR NEW.ioc='EST'
---     THEN SET NEW.ioc='USR';
---     END IF;
--- END; 
--- // 
--- DELIMITER ;
+DROP VIEW IF EXISTS topAces;
+CREATE VIEW topAces AS 
+SELECT p.name, SUM(m.ace) AS total_aces
+FROM plays AS m 
+JOIN player AS p ON m.player_id=p.player_id
+GROUP BY m.player_id
+ORDER BY SUM(m.ace) DESC
+LIMIT 10;
+
+SELECT * FROM topAces;
+
+
+
+DROP TRIGGER IF EXISTS onInsertionPlayer;
+DELIMITER // 
+CREATE TRIGGER onInsertionPlayer 
+BEFORE INSERT ON player 
+FOR EACH ROW 
+BEGIN 
+    IF NEW.ioc='RUS' OR NEW.ioc='EST'
+    THEN SET NEW.ioc='USR';
+    END IF;
+END; 
+// 
+DELIMITER ;
 
